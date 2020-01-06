786a5a3f99231ba6465982a9982754df01851f8a
ParkerBacall <pbacall2@gmail.com>
2020-01-06 14:26:00 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,12 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.valid?
     @post.update(post_params)
-
     redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
